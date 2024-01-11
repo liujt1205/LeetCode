d3ffd43e7c479880df2a5e817f72daf2e71ef325
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-01-10 19:24:20 -0600

    Time: 523 ms (93.99%), Space: 31.7 MB (32.04%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,12 +4,11 @@ class Solution:
         i = 0
         curSum = -float('inf')
         res = -float('inf')
-        while i < n:
+        for i in range(n):
             if curSum < 0:
                 curSum = nums[i]
             else:
                 curSum += nums[i]
             res = max(res, curSum)
-            i += 1
         return res
         
