e4f5b0252b930215d58ad6fe3220dda3aa94f973
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-02-03 23:03:25 -0600

    Time: 103 ms (66.07%), Space: 17.3 MB (76.65%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,53 +1,29 @@
 class Solution:
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
-        if not t or not s:
-            return ""
-
-        dict_t = Counter(t)
-
-        required = len(dict_t)
-
-        # Filter all the characters from s into a new list along with their index.
-        # The filtering criteria is that the character should be present in t.
-        filtered_s = []
-        for i, char in enumerate(s):
-            if char in dict_t:
-                filtered_s.append((i, char))
-
-        l, r = 0, 0
-        formed = 0
-        window_counts = {}
-
-        ans = float("inf"), None, None
-
-        # Look for the characters only in the filtered list instead of entire s. This helps to reduce our search.
-        # Hence, we follow the sliding window approach on as small list.
-        while r < len(filtered_s):
-            character = filtered_s[r][1]
-            window_counts[character] = window_counts.get(character, 0) + 1
-
-            if window_counts[character] == dict_t[character]:
-                formed += 1
-
-            # If the current window has all the characters in desired frequencies i.e. t is present in the window
-            while l <= r and formed == required:
-                character = filtered_s[l][1]
-
-                # Save the smallest window until now.
-                end = filtered_s[r][0]
-                start = filtered_s[l][0]
-                if end - start + 1 < ans[0]:
-                    ans = (end - start + 1, start, end)
-
-                window_counts[character] -= 1
-                if window_counts[character] < dict_t[character]:
-                    formed -= 1
-                l += 1    
-
-            r += 1    
-        return "" if ans[0] == float("inf") else s[ans[1] : ans[2] + 1]
+    def minWindow(self, s: str, t: str) -> str:
+        left = 0
+        right = 0
+        freq = {}
+        res = ""
+        count = 0
+        length = float('inf')
+        for char in t:
+            if freq.get(char, 0) == 0:
+                count += 1
+            freq[char] = freq.get(char, 0) - 1
+        while right < len(s):
+            cur = s[right]
+            freq[cur] = freq.get(cur, 0) + 1
+            if freq[cur] == 0:
+                count -= 1
+            while count == 0:
+                if right - left < length:
+                    res = s[left: right + 1]
+                    length = right - left
+                pre = s[left]
+                freq[pre] -= 1
+                if freq[pre] < 0:
+                    count += 1
+                left += 1
+            right += 1
+        return res
+                
