271bb70f9c076c6e2eb282af919d8fde5a2c9e51
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-02-07 23:48:40 -0600

    Time: 28 ms (93.58%), Space: 16.6 MB (71.24%) - LeetHub

--- 0276-paint-fence/0276-paint-fence.py
+++ 0276-paint-fence/0276-paint-fence.py
@@ -2,11 +2,12 @@ class Solution:
     def numWays(self, n: int, k: int) -> int:
         if n > 3 and k == 1:
             return 0
-        pre = [0] * 2
-        pre[0] = k
-        for i in range(1, n):
-            cur = [0] * 2
-            cur[0] = (pre[0] + pre[1]) * (k - 1)
-            cur[1] = pre[0]
-            pre = cur
-        return sum(pre)
+        if n == 1:
+            return k
+        pre = k
+        res = k * k
+        for i in range(3, n + 1):
+            cur = (k - 1) * (res + pre)
+            pre = res
+            res = cur
+        return res
