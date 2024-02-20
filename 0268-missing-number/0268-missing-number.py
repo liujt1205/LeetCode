af066b70e35545eb69ca256e368b5fc3874baf86
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-02-19 20:02:28 -0600

    Time: 111 ms (68.52%), Space: 17.7 MB (98.98%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,20 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        seenN = False
-        zeroIndex = -1
+        total = 0
         for num in nums:
-            index = abs(num)
-            if index == n:
-                seenN = True
-            elif nums[index] == 0:
-                zeroIndex = index
-            else:
-                nums[index] *= -1
-        if seenN == False:
-            return n
-        for i in range(n):
-            if nums[i] > 0:
-                return i
-            if nums[i] == 0 and zeroIndex == -1:
-                return i
+            total += num
+        return (1 + n) * n // 2 - total
