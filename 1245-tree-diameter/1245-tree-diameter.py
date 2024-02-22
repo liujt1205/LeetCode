f3e1f0998f2d8a9861e917dbd82b42251788117a
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-02-21 21:24:07 -0600

    Time: 162 ms (31.13%), Space: 21.5 MB (93.09%) - LeetHub

--- 1245-tree-diameter/1245-tree-diameter.py
+++ 1245-tree-diameter/1245-tree-diameter.py
@@ -2,32 +2,30 @@ class Solution:
     def treeDiameter(self, edges: List[List[int]]) -> int:
         if not edges:
             return 0
-        graph = {}
         n = len(edges) + 1
+        graph = [set() for _ in range(n)]
         for edge in edges:
             first, second = edge
-            graph.setdefault(first, [])
-            graph[first].append(second)
-            graph.setdefault(second, [])
-            graph[second].append(first)
+            graph[first].add(second)
+            graph[second].add(first)
         def findDistance(node, n):
-            distance = [-1] * n
-            queue = deque([(node, 0)])
+            queue = deque([node])
+            distance = -1
+            visited = [0] * n
+            visited[node] = 1
+            lastNode = node
             while queue:
-                m = len(queue)
-                for i in range(m):
-                    node, dis = queue.popleft()
-                    if distance[node] == -1:
-                        distance[node] = dis
-                        for neighbor in graph[node]:
-                            queue.append((neighbor, dis + 1))
-            biggest = 0
-            farNode = 0
-            for i in range(n):
-                if distance[i] > biggest:
-                    farNode = i
-                    biggest = distance[i]
-            return biggest, farNode
+                next_queue = deque()
+                while queue:
+                    cur = queue.popleft()
+                    for neighbor in graph[cur]:
+                        if visited[neighbor] == 0:
+                            visited[neighbor] = 1
+                            next_queue.append(neighbor)
+                            lastNode = neighbor
+                distance += 1
+                queue = next_queue
+            return distance, lastNode
         
         dis, far = findDistance(0, n)
         res, realFar = findDistance(far, n)
