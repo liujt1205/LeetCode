6c65c1f8bf31fd5f69b989ae8dffc37e5f9183ed
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-03-31 22:07:29 -0500

    Time: 39 ms (32.99%), Space: 16.6 MB (59.58%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -2,7 +2,7 @@ class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
         i = 0
         j = 0
-        while i < len(abbr):
+        while i < len(abbr) and j < len(word):
             if abbr[i].isdigit():
                 cur = int(abbr[i])
                 if cur == 0:
@@ -13,13 +13,9 @@ class Solution:
                     i += 1
                 j += cur
             else:
-                if j < len(word) and abbr[i] == word[j]:
+                if abbr[i] == word[j]:
                     i += 1
                     j += 1
                 else:
                     return False
-        
-        if j == len(word):
-            return True
-        else:
-            return False
+        return i == len(abbr) and j == len(word)
