d671e3f3e34830aa2c86d8e352c46f6ea86e096f
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-04-02 22:08:07 -0500

    Time: 3288 ms (81.72%), Space: 16.6 MB (71.59%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,26 +1,21 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
-        def helper(board, visited, word, row, col, i):
-            if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]) or visited[row][col] == 1 or board[row][col] != word[i]:
-                return False
-            visited[row][col] = 1
-            if i == len(word) - 1:
+        def helper(board, word, row, col, i):
+            if i == len(word):
                 return True
-            res = False
-            for direction in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
-                newRow = row + direction[0]
-                newCol = col + direction[1]
-                res = res or helper(board, visited, word, newRow, newCol, i + 1)
-                if res:
-                    return True
-            visited[row][col] = 0
+            if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]) or board[row][col] != word[i]:
+                return False
+            cur = board[row][col]
+            board[row][col] = '#'
+            if helper(board, word, row+1, col, i+1) or helper(board, word, row-1, col, i+1) or helper(board, word, row, col+1, i+1) or helper(board, word, row, col-1, i+1):
+                return True
+            board[row][col] = cur
             return False
                 
         
         for row in range(len(board)):
             for col in range(len(board[0])):
-                visited = [[0] * len(board[0]) for _ in range(len(board))]
-                if helper(board, visited, word, row, col, 0):
+                if helper(board, word, row, col, 0):
                     return True
         return False
