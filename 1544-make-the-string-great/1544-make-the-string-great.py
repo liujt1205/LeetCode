6d9694664de5a08afaffcc5b6e0357bb1443e394
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-04-04 20:32:42 -0500

    Time: 33 ms (83.07%), Space: 16.5 MB (46.83%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -2,9 +2,7 @@ class Solution:
     def makeGood(self, s: str) -> str:
         stack = []
         for char in s:
-            if not stack:
-                stack.append(char)
-            elif abs(ord(char) - ord(stack[-1])) == 32:
+            if stack and abs(ord(char) - ord(stack[-1])) == 32:
                 stack.pop()
             else:
                 stack.append(char)
