5ecf4abd780ec1bed4c4afe3fa71b38596a1dc36
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-04-13 06:47:43 -0700

    Time: 222 ms (56.27%), Space: 17.9 MB (34.16%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -1,26 +1,27 @@
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        rows = len(matrix)
-        cols = len(matrix[0])
-        memo = [[0] * cols for _ in range(rows)]
-        for i in range(rows-1, -1, -1):
-            count = 0
-            for j in range(cols-1, -1, -1):
-                if matrix[i][j] == "1":
-                    count += 1
+        if not matrix: return 0
+        m = len(matrix)
+        n = len(matrix[0])
+        left = [0] * n 
+        right = [n] * n 
+        height = [0] * n
+        maxarea = 0
+        for i in range(m):
+            cur_left, cur_right = 0, n
+            for j in range(n):
+                if matrix[i][j] == '1': height[j] += 1
+                else: height[j] = 0
+            for j in range(n):
+                if matrix[i][j] == '1': left[j] = max(left[j], cur_left)
                 else:
-                    count = 0
-                memo[i][j] = count
-        
-        res = 0
-        for row in range(rows):
-            for col in range(cols):
-                if matrix[row][col] == "1":
-                    width = cols
-                    height = 1
-                    while row+height-1 < rows and width != 0:
-                        width = min(width, memo[row+height-1][col])
-                        res = max(res, width * height)
-                        height += 1
-                        
-        return res
+                    left[j] = 0
+                    cur_left = j + 1
+            for j in range(n-1, -1, -1):
+                if matrix[i][j] == '1': right[j] = min(right[j], cur_right)
+                else:
+                    right[j] = n
+                    cur_right = j
+            for j in range(n):
+                maxarea = max(maxarea, height[j] * (right[j] - left[j]))
+        return maxarea
