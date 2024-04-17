8445a150c1b26fab7635f75b995e700cd9732568
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-04-17 10:19:30 +0800

    Time: 45 ms (46.67%), Space: 18.1 MB (10.83%) - LeetHub

---
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+        res = None
+        queue = deque()
+        queue.append((root, ""))
+        while queue:
+            cur, prefix = queue.popleft()
+            newPrefix = chr(ord('a') + cur.val) + prefix
+            if not cur.left and not cur.right:
+                if not res:
+                    res = newPrefix
+                elif res > newPrefix:
+                    res = newPrefix
+            else:
+                if cur.left:
+                    queue.append((cur.left, newPrefix))
+                if cur.right:
+                    queue.append((cur.right, newPrefix))
+        return res
+        
