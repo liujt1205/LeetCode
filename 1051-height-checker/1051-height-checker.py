20d9287ddbb8e70b983ea4ce978950c20c06b449
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-06-10 08:56:03 +0800

    Time: 35 ms (80.35%), Space: 16.4 MB (77.64%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -1,6 +1,14 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        correct = sorted(heights)
+        count = defaultdict(int)
+        for num in heights:
+            count[num] += 1
+        minH, maxH = min(heights), max(heights)
+        correct = []
+        for i in range(minH, maxH + 1):
+            while count[i] > 0:
+                correct.append(i)
+                count[i] -= 1
         res = 0
         for i in range(len(heights)):
             if heights[i] != correct[i]:
