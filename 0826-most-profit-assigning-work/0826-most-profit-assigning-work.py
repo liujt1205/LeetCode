3fd8f9b47f90e5b8b94ddb2a78f8ef206a967518
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-06-18 17:53:50 +0800

    Time: 263 ms (87.12%), Space: 19.6 MB (56.95%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -1,16 +1,14 @@
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
         worker.sort()
-        proj = []
-        for i in range(len(difficulty)):
-            proj.append((-profit[i], difficulty[i]))
-        heapq.heapify(proj)
+        sortJobs = sorted(zip(difficulty, profit))
+        jobIndex = 0
         res = 0
-        index = len(worker) - 1
-        while proj and index >= 0:
-            cur = heapq.heappop(proj)
-            curProf, curDiff = -cur[0], cur[1]
-            while index >= 0 and worker[index] >= curDiff:
-                res += curProf
-                index -= 1
+        curMax = 0
+        for cap in worker:
+            while jobIndex < len(sortJobs) and sortJobs[jobIndex][0] <= cap:
+                curMax = max(curMax, sortJobs[jobIndex][1])
+                jobIndex += 1
+            res += curMax
         return res
+                
