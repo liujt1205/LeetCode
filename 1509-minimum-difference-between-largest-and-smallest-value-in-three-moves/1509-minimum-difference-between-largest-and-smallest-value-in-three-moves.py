f348822071df092fda3dc31de5f68bcb3c5e7da7
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-07-03 12:04:36 +0800

    Time: 240 ms (99.06%), Space: 27.4 MB (39.86%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -2,19 +2,9 @@ class Solution:
     def minDifference(self, nums: List[int]) -> int:
         if len(nums) <= 4:
             return 0
-        large = []
-        small = []
-        for num in nums:
-            heapq.heappush(large, num)
-            heapq.heappush(small, -num)
-            if len(large) > 4:
-                heapq.heappop(large)
-            if len(small) > 4:
-                heapq.heappop(small)
-        large.sort()
-        newSmall = [-i for i in small]
-        newSmall.sort()
+        small = sorted(heapq.nsmallest(4, nums))
+        large = sorted(heapq.nlargest(4, nums))
         res = float('inf')
         for i in range(4):
-            res = min(res, large[i] - newSmall[i])
+            res = min(res, large[i] - small[i])
         return res
