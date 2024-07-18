1715d18f81746397ed3515ffe0fd4c0a8268e6af
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-07-18 12:48:23 +0800

    Time: 200 ms (91.65%), Space: 17.3 MB (99.52%) - LeetHub

---
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        res = 0
+        if x >= y:
+            first = "a"
+            second = "b"
+            larger = x
+            smaller = y
+        else:
+            first = "b"
+            second = "a"
+            larger = y
+            smaller = x
+        
+        count = [0] * 2
+        for char in s:
+            if char == first:
+                count[0] += 1
+            elif char == second:
+                if count[0] > 0:
+                    count[0] -= 1
+                    res += larger
+                else:
+                    count[1] += 1
+            else:
+                if count[0] * count[1] > 0:
+                    res += smaller * min(count[0], count[1])
+                count = [0] * 2
+        
+        res += smaller * min(count[0], count[1])
+        return res
+                
