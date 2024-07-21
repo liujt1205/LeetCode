c4e49c908967b43431fe0bca46d631226bdab93c
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-07-21 11:52:10 +0800

    Time: 567 ms (91.60%), Space: 25.4 MB (45.80%) - LeetHub

---
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.py
@@ -0,0 +1,37 @@
+class Solution:
+    def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
+        rowOrder = defaultdict(list)
+        colOrder = defaultdict(list)
+        
+        def getOrder(conditions, orderDict, k):
+            indegree = [0] * (k + 1)
+            posBelow = defaultdict(list)
+            for above, below in conditions:
+                posBelow[above].append(below)
+                indegree[below] += 1
+            queue = deque()
+            for i in range(1, k + 1):
+                if indegree[i] == 0:
+                    queue.append(i)
+            pos = 0
+            while queue:
+                cur = queue.popleft()
+                orderDict[cur] = pos
+                pos += 1
+                for below in posBelow[cur]:
+                    indegree[below] -= 1
+                    if indegree[below] == 0:
+                        queue.append(below)
+            return pos == k
+        
+        if getOrder(rowConditions, rowOrder, k) and getOrder(colConditions, colOrder, k):
+            res = [[0] * k for _ in range(k)]
+            for i in range(1, k + 1):
+                rowIndex = rowOrder[i]
+                colIndex = colOrder[i]
+                res[rowIndex][colIndex] = i
+            return res
+        else:
+            print(list(rowOrder))
+            print(list(colOrder))
+            return []
