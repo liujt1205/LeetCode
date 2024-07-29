6c55c51dae5d7f3a21d5709a8947202232d874ee
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-07-29 09:45:22 +0800

    Time: 593 ms (74.49%), Space: 40.1 MB (65.31%) - LeetHub

--- 1230-toss-strange-coins/1230-toss-strange-coins.py
+++ 1230-toss-strange-coins/1230-toss-strange-coins.py
@@ -5,7 +5,7 @@ class Solution:
         memo[0][1] = prob[0]
         memo[0][0] = 1 - prob[0]
         for i in range(1, n):
-            for j in range(i + 2):
+            for j in range(min(i + 2, target + 1)):
                 if j - 1 >= 0:
                     memo[i][j] += prob[i] * memo[i - 1][j - 1]
                 memo[i][j] += (1 - prob[i]) * memo[i - 1][j]
