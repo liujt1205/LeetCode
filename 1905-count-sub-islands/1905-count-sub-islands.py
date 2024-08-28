81bf6a1b29b35aea4ab2425990da3b120f71d759
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-08-27 21:17:30 -0500

    Time: 2004 ms (78.93%), Space: 26.6 MB (90.27%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -0,0 +1,30 @@
+class Solution:
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        queue = deque()
+        m, n = len(grid1), len(grid1[0])
+        
+        def sink(row, col, grid2):
+            if row < 0 or row >= m or col < 0 or col >= n or grid2[row][col] == 0:
+                return
+            grid2[row][col] = 0
+            sink(row - 1, col, grid2)
+            sink(row + 1, col, grid2)
+            sink(row, col - 1, grid2)
+            sink(row, col + 1, grid2)
+
+        for row in range(m):
+            for col in range(n):
+                if grid2[row][col] > 0 and grid1[row][col] > 0:
+                    grid2[row][col] = 2
+                elif grid2[row][col] > 0 and grid1[row][col] == 0:
+                    sink(row, col, grid2)
+        
+        res = 0
+        
+        for row in range(m):
+            for col in range(n):
+                if grid2[row][col] > 0:
+                    res += 1
+                    sink(row, col, grid2)
+        
+        return res
