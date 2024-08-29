9fc6a975e6cf8ed13ff04c3476968f4b8ecd8796
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-08-28 22:27:39 -0500

    Time: 35 ms (68.82%), Space: 16.5 MB (91.85%) - LeetHub

--- 0666-path-sum-iv/0666-path-sum-iv.py
+++ 0666-path-sum-iv/0666-path-sum-iv.py
@@ -1,29 +1,30 @@
 class Solution:
     def pathSum(self, nums: List[int]) -> int:
         pre = [0] * 9
-        cur = [-1] * 9
-        level = 1
+        cur = [0] * 9
+        level = 0
         res = 0
+        def consolidate():
+            nonlocal res, pre, cur
+            for i in range(9):
+                if cur[i] != -1:
+                    cur[i] += pre[(i + 1) // 2]
+                elif i % 2 == 1 and cur[i + 1] == -1 and pre[(i + 1) // 2] != -1:
+                    res += pre[(i + 1) // 2]
+            pre = cur[:]
+            cur = [-1] * 9
+            
         for i in range(len(nums)):
             curNode = nums[i]
             curLevel, curPos, curVal = curNode // 100, (curNode // 10) % 10, curNode % 10
-            if curLevel == level:
-                cur[curPos] = curVal
-            else:
-                for j in range(9):
-                    if cur[j] != -1:
-                        cur[j] += pre[(j + 1) // 2]
-                    elif j % 2 == 1 and cur[j + 1] == -1 and pre[(j + 1) // 2] != -1:
-                        res += pre[(j + 1) // 2]
-                pre = cur[:]
-                cur = [-1] * 9
+            if curLevel != level:
+                consolidate()
                 level = curLevel
-                cur[curPos] = curVal
+            cur[curPos] = curVal
+        
+        consolidate()
         for i in range(9):
-            if cur[i] != -1:
-                cur[i] += pre[(i + 1) // 2]
-                res += cur[i]
-            elif i % 2 == 1 and cur[i + 1] == -1 and pre[(i + 1) // 2] != -1:
-                res += pre[(i + 1) // 2]
+            if pre[i] != -1:
+                res += pre[i]
         
         return res
