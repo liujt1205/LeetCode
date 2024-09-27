f730c436f115b6a05219b6ff0c755245630004c9
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-09-26 19:40:45 -0500

    Time: 1309 ms (31.04%), Space: 17.9 MB (15.97%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -0,0 +1,29 @@
+from sortedcontainers import SortedDict
+
+class MyCalendarTwo:
+
+    def __init__(self):
+        self.booking_count = SortedDict()
+        self.max_overlapped_booking = 2
+
+    def book(self, start: int, end: int) -> bool:
+        self.booking_count[start] = self.booking_count.get(start, 0) + 1
+        self.booking_count[end] = self.booking_count.get(end, 0) - 1
+        overlapped_booking = 0
+        for count in self.booking_count.values():
+            overlapped_booking += count
+            if overlapped_booking > self.max_overlapped_booking:
+                self.booking_count[start] -= 1
+                self.booking_count[end] += 1
+
+                if self.booking_count[start] == 0:
+                    del self.booking_count[start]
+
+                return False
+
+        return True
+
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(start,end)
