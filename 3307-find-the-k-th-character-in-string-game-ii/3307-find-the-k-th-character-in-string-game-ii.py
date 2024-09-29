93f8effdb27044305908b2bc07eb6328241ec97c
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-09-29 13:11:54 -0500

    Time: 48 ms (100.00%), Space: 16.7 MB (100.00%) - LeetHub

---
+++ 3307-find-the-k-th-character-in-string-game-ii/3307-find-the-k-th-character-in-string-game-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def kthCharacter(self, k: int, operations: List[int]) -> str:
+        chars = "abcdefghijklmnopqrstuvwxyz"
+        length = [2 ** i for i in range(len(operations) + 1)]
+        res = 0
+        while k > 1:
+            index = bisect_left(length, k)
+            if index <= 0:
+                break
+            else:
+                next_len = length[index - 1]
+                k -= next_len
+                res += operations[index - 1]
+        
+        return chars[res % 26]
