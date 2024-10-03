dde6253e14996ae30c3aafe2f158838cfee5eed5
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-10-03 09:51:34 -0500

    Time: 420 ms (71.93%), Space: 17.2 MB (66.38%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        memo = [[n * n + 1] * n for _ in range(n)]
+        queue = deque()
+        queue.append((0, 0, 0))
+        while queue:
+            row, col, step = queue.popleft()
+            if not 0 <= row < n or not 0 <= col < n or grid[row][col] == 1 or memo[row][col] <= step + 1:
+                continue
+            memo[row][col] = step + 1
+            queue.append((row - 1, col - 1, step + 1))
+            queue.append((row - 1, col, step + 1))
+            queue.append((row - 1, col + 1, step + 1))
+            queue.append((row, col - 1, step + 1))
+            queue.append((row, col + 1, step + 1))
+            queue.append((row + 1, col - 1, step + 1))
+            queue.append((row + 1, col, step + 1))
+            queue.append((row + 1, col + 1, step + 1))
+
+        return memo[n - 1][n - 1] if memo[n - 1][n - 1] != n * n + 1 else -1
