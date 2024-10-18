3aa6e7f5193cf14a13fd37acecf3ae7ffd914374
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-10-18 17:46:09 -0500

    Time: 35 ms (100.00%), Space: 17.9 MB (8.46%) - LeetHub

---
+++ 0986-interval-list-intersections/0986-interval-list-intersections.py
@@ -0,0 +1,24 @@
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        memo = defaultdict(int)
+        for start, end in firstList:
+            memo[start] += 1
+            memo[end + 0.5] -= 1
+            
+        for start, end in secondList:
+            memo[start] += 1
+            memo[end + 0.5] -= 1
+            
+        res = []
+        count = 0
+        start = -1
+        for time in sorted(memo.keys()):
+            count += memo[time]
+            print(time, count)
+            if count == 2:
+                start = time
+            elif start >= 0 and count < 2:
+                res.append([start, int(time - 0.5)])
+                start = -1
+        
+        return res
