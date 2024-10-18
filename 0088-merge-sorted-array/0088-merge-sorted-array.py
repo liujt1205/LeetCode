64590f412fbec66972b48afd811f7dcaae0784d5
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-10-18 16:45:50 -0500

    Time: 0 ms (100.00%), Space: 16.6 MB (63.00%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -9,13 +9,11 @@ class Solution:
             if i < 0:
                 nums1[k] = nums2[j]
                 j -= 1
-            elif j < 0:
+            elif j < 0 or nums2[j] <= nums1[i]:
                 nums1[k] = nums1[i]
                 i -= 1
-            elif nums2[j] > nums1[i]:
+            else:
                 nums1[k] = nums2[j]
                 j -= 1
-            else:
-                nums1[k] = nums1[i]
-                i -= 1
+
                 
