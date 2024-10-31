6ba757423290d1c34e28beefd612d7c22467361f
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-10-31 16:33:06 -0500

    Time: 281 ms (28.47%), Space: 25.8 MB (85.68%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
@@ -1,15 +1,17 @@
 class Solution:
     def maxMoves(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        memo = [[0] * n for _ in range(m + 2)]
+        prev = [0] * m
         res = 0
         for col in range(1, n):
-            for row in range(1, m + 1):
-                if row - 2 >= 0 and grid[row - 1][col] > grid[row - 2][col - 1] and (col - 1 == 0 or memo[row - 2][col - 1] != 0):
-                    memo[row - 1][col] = max(memo[row - 1][col], memo[row - 2][col - 1] + 1)
-                if grid[row - 1][col] > grid[row - 1][col - 1]  and (col - 1 == 0 or memo[row - 1][col - 1] != 0):
-                    memo[row - 1][col] = max(memo[row - 1][col], memo[row - 1][col - 1] + 1)
-                if row < m and grid[row - 1][col] > grid[row][col - 1]  and (col - 1 == 0 or memo[row][col - 1] != 0):
-                    memo[row - 1][col] = max(memo[row - 1][col], memo[row][col - 1] + 1)
-                res = max(res, memo[row - 1][col])
+            cur = [0] * m
+            for row in range(m):
+                if row - 1 >= 0 and grid[row][col] > grid[row - 1][col - 1] and (col - 1 == 0 or prev[row - 1] != 0):
+                    cur[row] = max(cur[row], prev[row - 1] + 1)
+                if grid[row][col] > grid[row][col - 1]  and (col - 1 == 0 or prev[row] != 0):
+                    cur[row] = max(cur[row], prev[row] + 1)
+                if row + 1 < m and grid[row][col] > grid[row + 1][col - 1]  and (col - 1 == 0 or prev[row + 1] != 0):
+                    cur[row] = max(cur[row], prev[row + 1] + 1)
+                res = max(res, cur[row])
+            prev = cur
         return res
