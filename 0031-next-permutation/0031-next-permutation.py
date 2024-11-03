40c9cad45312b2baa3e8331faedfaa5143db46e2
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-11-03 08:46:48 -0600

    Time: 0 ms (100.00%), Space: 16.7 MB (24.77%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,35 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def findNextBigger(nums, start, target):
+            end = len(nums) - 1
+            while start <= end:
+                mid = start + (end - start) // 2
+                if nums[mid] <= target:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            
+            return start - 1
+        
+        def reverseList(nums, start):
+            i = 0
+            while start + i < len(nums) - i:
+                nums[start + i], nums[len(nums) - 1 - i] = nums[len(nums) - 1 - i], nums[start + i]
+                i += 1
+        
+        i = len(nums) - 1
+        while i > 0:
+            if nums[i - 1] < nums[i]:
+                index = findNextBigger(nums, i, nums[i - 1])
+                nums[i - 1], nums[index] = nums[index], nums[i - 1]
+                reverseList(nums, i)
+                break
+            i -= 1
+                
+        if i == 0:
+            reverseList(nums, 0)
+        
+            
