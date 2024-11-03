73b96a8b9bace46d3856b5b931589cc54685e6c7
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-11-02 23:12:31 -0500

    Time: 11 ms (40.00%), Space: 27.4 MB (88.97%) - LeetHub

---
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isGoodArray(self, nums: List[int]) -> bool:
+        gcd = nums[0]
+        for a in nums:
+            while a:
+                gcd, a = a, gcd % a
+        return gcd == 1
