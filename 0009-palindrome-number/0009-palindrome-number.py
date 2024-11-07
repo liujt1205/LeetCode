3f2172df33024ab147b1a62b9917f8dccdf47693
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-11-06 23:38:35 -0600

    Time: 14 ms (21.41%), Space: 16.6 MB (64.29%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        
+        digits = list(str(x))
+        for i in range((len(digits) + 1) // 2):
+            if digits[i] != digits[len(digits) - 1 - i]:
+                return False
+            
+        return True
