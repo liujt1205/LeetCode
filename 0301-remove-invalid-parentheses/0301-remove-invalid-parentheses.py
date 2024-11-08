6f6b450916ec51b423c04675afff7a04e3fe19bb
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-11-08 10:45:26 -0600

    Time: 1160 ms (42.96%), Space: 17.1 MB (42.42%) - LeetHub

---
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
@@ -0,0 +1,44 @@
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        r_right = 0
+        count = 0
+        for char in s:
+            if char not in "()":
+                continue
+            if char == '(':
+                count += 1
+            elif count == 0:
+                r_right += 1
+            else:
+                count -= 1
+                
+        r_left = count
+        min_r = r_left + r_right
+        res = []
+        
+        def helper(cur_string, index, left, right, count):
+            if index == len(s):
+                if count == 0:
+                    print(count, "".join(cur_string))
+                    res.append("".join(cur_string))
+                return
+            if s[index] not in "()":
+                cur_string.append(s[index])
+                helper(cur_string, index + 1, left, right, count)
+                cur_string.pop()
+                return
+            if s[index] == '(':
+                cur_string.append('(')
+                helper(cur_string, index + 1, left, right, count + 1)
+                cur_string.pop()
+                helper(cur_string, index + 1, left + 1, right, count)
+            else:
+                if count > 0:
+                    cur_string.append(')')
+                    helper(cur_string, index + 1, left, right, count - 1)
+                    cur_string.pop()
+                if right < r_right:
+                    helper(cur_string, index + 1, left, right + 1, count)
+                    
+        helper([], 0, 0, 0, 0)
+        return list(set(res))
