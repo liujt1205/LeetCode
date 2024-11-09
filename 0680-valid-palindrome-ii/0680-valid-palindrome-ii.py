7d837aca9d457205647221753d5d5c76789cfe13
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-11-09 12:09:37 -0600

    Time: 59 ms (54.41%), Space: 17.1 MB (28.53%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,16 +1,19 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def helper(s, start, end, deleted):
-            if start > end:
-                return True
-            elif s[start] == s[end]:
-                start += 1
-                end -= 1
-                return helper(s, start, end, deleted)
-            else:
-                if deleted:
+        def validate(s, l, r):
+            while l < r:
+                if s[l] != s[r]:
                     return False
-                else:
-                    return helper(s, start + 1, end, True) or helper(s, start, end - 1, True)
+                l += 1
+                r -= 1
+            return True
+        
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                return validate(s, l + 1, r) or validate(s, l, r - 1)
+            l += 1
+            r -= 1
                 
-        return helper(s, 0, len(s) - 1, False)
+        return True
