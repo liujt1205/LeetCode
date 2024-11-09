b928d14df5123caa8a6f28666c8d2a6789611aaa
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-11-09 16:45:32 -0600

    Time: 3 ms (14.92%), Space: 16.6 MB (58.63%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -3,7 +3,6 @@ class Solution:
         orders = [27] * 26
         for i in range(len(order)):
             orders[ord(order[i]) - ord('a')] = i
-        def customSort(char):
-            return orders[ord(char) - ord('a')]
-        sortedS = ''.join(sorted(s, key=customSort))
+        
+        sortedS = ''.join(sorted(s, key=lambda char: orders[ord(char) - ord('a')]))
         return sortedS
