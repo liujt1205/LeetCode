601c11c329b1d72c8ef9ce44427abb6660558fc3
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-11-12 20:57:29 -0600

    Time: 120 ms (75.60%), Space: 30.5 MB (97.40%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.py
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.py
@@ -1,18 +1,28 @@
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
         nums.sort()
-        def getPairWithSumSmaller(nums, target):
-            left = 0
-            right = len(nums) - 1
-            res = 0
-            while left < right:
-                curSum = nums[left] + nums[right]
-                if curSum < target:
-                    res += right - left
-                    left += 1
+
+        def findSmallestLargerThan(nums, target):
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] >= target:
+                    right = mid - 1
                 else:
-                    right -= 1
-            return res
+                    left = mid + 1
+            return left
         
-        return getPairWithSumSmaller(nums, upper + 1) - getPairWithSumSmaller(nums, lower)
+        biggest = nums[-1]
+        low = findSmallestLargerThan(nums, lower - biggest)
+        high = findSmallestLargerThan(nums, upper - biggest)
+        res = 0
+        for i in range(len(nums) - 1, -1, -1):
+            while high < i and nums[high] + nums[i] <= upper:
+                high += 1
+            while low < i and nums[low] + nums[i] < lower:
+                low += 1
+            if low >= i:
+                break
+            res += min(i, high) - low
             
+        return res
