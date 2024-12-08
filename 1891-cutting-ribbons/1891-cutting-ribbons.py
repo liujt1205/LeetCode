1b446c2206b5d4900e4f7462bd7c5e7463339490
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2024-12-07 18:31:25 -0600

    Time: 415 ms (18.98%), Space: 29.7 MB (20.90%) - LeetHub

--- 1891-cutting-ribbons/1891-cutting-ribbons.py
+++ 1891-cutting-ribbons/1891-cutting-ribbons.py
@@ -1,7 +1,5 @@
 class Solution:
     def maxLength(self, ribbons: List[int], k: int) -> int:
-        if sum(ribbons) < k:
-            return 0
         shortest, longest = 1, max(ribbons)
         while shortest <= longest:
             mid = shortest + (longest - shortest) // 2
