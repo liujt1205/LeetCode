949e84924d9750ee784b3e828c0ce3ddfc9ca694
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2025-02-28 00:37:13 -0600

    Time: 469 ms (39.79%), Space: 56.5 MB (23.22%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
@@ -1,25 +1,46 @@
 class Solution:
     def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
-        def lcs(A, B):
-            n, m = len(A), len(B)
-            dp = [["" for _ in range(m + 1)] for _ in range(n + 1)]
-            for i in range(n):
-                for j in range(m):
-                    if A[i] == B[j]:
-                        dp[i + 1][j + 1] = dp[i][j] + A[i]
-                    else:
-                        dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1], key=len)
-            return dp[-1][-1]
+        str1_length = len(str1)
+        str2_length = len(str2)
 
-        res, i, j = "", 0, 0
-        for c in lcs(str1, str2):
-            while str1[i] != c:
-                res += str1[i]
-                i += 1
-            while str2[j] != c:
-                res += str2[j]
-                j += 1
-            res += c
-            i, j = i + 1, j + 1
-        return res + str1[i:] + str2[j:]
+        dp = [
+            [0 for _ in range(str2_length + 1)] for _ in range(str1_length + 1)
+        ]
+
+        for row in range(str1_length + 1):
+            dp[row][0] = row
+
+        for col in range(str2_length + 1):
+            dp[0][col] = col
+
+        for row in range(1, str1_length + 1):
+            for col in range(1, str2_length + 1):
+                if str1[row - 1] == str2[col - 1]:
+                    dp[row][col] = dp[row - 1][col - 1] + 1
+                else:
+                    dp[row][col] = min(dp[row - 1][col], dp[row][col - 1]) + 1
+
+        super_sequence = []
+        row, col = str1_length, str2_length
+
+        while row > 0 and col > 0:
+            if str1[row - 1] == str2[col - 1]:
+                super_sequence.append(str1[row - 1])
+                row -= 1
+                col -= 1
+            elif dp[row - 1][col] < dp[row][col - 1]:
+                super_sequence.append(str1[row - 1])
+                row -= 1
+            else:
+                super_sequence.append(str2[col - 1])
+                col -= 1
+
+        while row > 0:
+            super_sequence.append(str1[row - 1])
+            row -= 1
+        while col > 0:
+            super_sequence.append(str2[col - 1])
+            col -= 1
+
+        return "".join(super_sequence[::-1])
             
