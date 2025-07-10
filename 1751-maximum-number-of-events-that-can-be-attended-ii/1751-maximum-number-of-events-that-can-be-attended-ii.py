0ec042e303fe3b1e713f9a562d7f34ba660ef120
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2025-07-10 03:07:23 -0400

    Time: 997 ms (34.64%), Space: 71.7 MB (73.13%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -5,8 +5,8 @@ class Solution:
         dp = [[0] * (n + 1) for _ in range(k + 1)]
         starts = [start for start, end, value in events]
 
-        for i in range(n - 1, -1, -1):
-            for count in range(1, k + 1):
+        for count in range(1, k + 1):
+            for i in range(n - 1, -1, -1):
                 next_i = bisect_right(starts, events[i][1])
                 dp[count][i] = max(dp[count][i + 1], events[i][2] + dp[count - 1][next_i])
 
