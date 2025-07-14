2d8a7d642afaeea6250f249ead9eacb188851a7d
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2025-07-14 19:58:25 -0400

    Time: 976 ms (17.35%), Space: 17.7 MB (81.75%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -4,9 +4,10 @@ class Solution:
         nums.sort()
         start = bisect_right(nums, 0)
         for i in range(start, len(nums) - 2):
+            k = i + 2
             for j in range(i + 1, len(nums) - 1):
-                largest = nums[i] + nums[j]
-                index = bisect_left(nums, largest)
-                res += index - j - 1
+                while k < len(nums) and nums[k] < nums[i] + nums[j]:
+                    k += 1
+                res += k - j - 1
 
         return res
