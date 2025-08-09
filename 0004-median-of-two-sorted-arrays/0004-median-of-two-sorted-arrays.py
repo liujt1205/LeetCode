65b4581e648a6fa4244bb5f9975382880ea50ec5
Jingtian Liu <90082188+liujt1205@users.noreply.github.com>
2025-08-09 03:09:50 -0400

    Time: 0 ms (100%), Space: 18 MB (82.63%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -4,24 +4,30 @@ class Solution:
             return self.findMedianSortedArrays(nums2, nums1)
 
         m, n = len(nums1), len(nums2)
-        left, right = 0, m
-
-        while left <= right:
-            partitionA = (left + right) // 2
-            partitionB = (m + n + 1) // 2 - partitionA
-
-            maxLeftA = float('-inf') if partitionA == 0 else nums1[partitionA - 1]
-            minRightA = float('inf') if partitionA == m else nums1[partitionA]
-            maxLeftB = float('-inf') if partitionB == 0 else nums2[partitionB - 1]
-            minRightB = float('inf') if partitionB == n else nums2[partitionB]
-
-            if maxLeftA <= minRightB and maxLeftB <= minRightA:
-                if (m + n) % 2 == 0:
-                    return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2
-                else:
-                    return max(maxLeftA, maxLeftB)
-            elif maxLeftA > minRightB:
-                right = partitionA - 1
+        if n == 0:
+            return None
+        if m == 0:
+            mid = n // 2
+            if n % 2 != 0:
+                return nums2[mid]
             else:
-                left = partitionA + 1
-                
+                return (nums2[mid] + nums2[mid - 1]) / 2
+
+        left, right = 0, m
+        while left <= right:
+            mid = (left + right) // 2
+            mid2 = (m + n + 1) // 2 - mid
+            left1 = -float('inf') if mid == 0 else nums1[mid - 1]
+            right1 = float('inf') if mid == m else nums1[mid]
+            left2 = -float('inf') if mid2 == 0 else nums2[mid2 - 1]
+            right2 = float('inf') if mid2 == n else nums2[mid2]
+
+            if left1 <= right2 and left2 <= right1:
+                if (m + n) % 2 == 0:
+                    return (max(left1, left2) + min(right1, right2)) / 2
+                else:
+                    return max(left1, left2)
+            elif left1 > right2:
+                right = mid - 1
+            else:
+                left = mid + 1
